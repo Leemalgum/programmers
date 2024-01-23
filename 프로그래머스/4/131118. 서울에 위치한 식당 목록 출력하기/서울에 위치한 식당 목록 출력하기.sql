a66876b133f7644f99b8878c3c294d5d0e869779
이맑음 <60692335+Leemalgum@users.noreply.github.com>
2024-01-23 18:14:24 +0900

    Update 서울에 위치한 식당 목록 출력하기.sql

--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -4,5 +4,11 @@ from rest_info i join rest_review r
 on i.rest_id = r.rest_id
 group by i.rest_id
 having address like '서울%'
--- where i.address like '서울%'
 order by score desc, favorites desc;
+
+/*
+SELECT i.rest_id, rest_name, food_type, favorites, address, round(avg(review_score), 2) score
+from rest_info i, rest_review r
+where i.rest_id = r.rest_id and i.address like ‘서울%’
+order by score desc, favorites desc;
+*/
